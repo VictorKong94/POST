31d42445c1652778a26900f460dfec53a25f4057
Victor Kong <victorkong94@gmail.com>
2016-06-15 23:32:11 -0700

    Update preprocessing of TSH and FG data

--- post_project_1/TSH_FG_study.R
+++ post_project_1/TSH_FG_study.R
@@ -1,35 +1,93 @@
+# Setup
+options(stringsAsFactors = F)
+
 # Read in glucose and TSH test data
-glucose_case = read.csv("diab_CASE_glucose.csv")
-glucose_cntl = read.csv("diab_CNTL_glucose.csv")
-tsh_case = read.csv("diab_CASE_TSH.csv")
-tsh_cntl = read.csv("diab_CNTL_TSH.csv")
+glucoseCase = read.csv("data/case_glucose.csv")
+glucoseCntl = read.csv("data/cntl_glucose.csv")
+tshCase = read.csv("data/case_TSH.csv")
+tshCntl = read.csv("data/cntl_TSH.csv")
+rxCase = read.csv("data/case_all_rx_processed.csv")
+rxCntl = read.csv("data/cntl_all_rx_processed.csv")
 
 # Compile data frame that contains all relevant information:
 # - id: the patient's specific ID number,
-# - group: the cohort to which the patient belongs,
-# - tsh_result: the patient's TSH test result (mU/L),
-# - deltafg: difference between mean poststatin FG and mean prestatin FG,
-# - maxdelta: difference between highest poststatin FG and mean prestatin FG,
-# - max2delta: difference between mean of two highest poststatin FGs and mean
-#              prestatin FG
+# - cohort: the cohort to which the patient belongs,
+# - tshResult: the patient's TSH test result (mU/L),
+# - avgBaseFG: the patient's average pre-Statin FG
+# - deltaFG: difference between mean poststatin FG and mean prestatin FG,
+# - maxDelta: difference between highest poststatin FG and mean prestatin FG,
+# - sex: patient's sex
+# - age: patient's age in years
+# - race: race with which patient identifies
+# - hispanic: whether patient identifies as hispanic
+# - diabetes: whether a patient was diagnosed as diabetic
+# - diagFromFG: whether a patient was diagnosed as diabetic by the FG method
+# - diagFromICD: whether a patient was diagnosed as diabetic by ICD standards
+# - group: code that corresponds categories of diabetic, prediabetic, etc.
 data = data.frame(id = integer(0),
-                  group = factor(NULL, levels = c("case", "cntl")),
-                  tsh_result = numeric(0),
-                  deltafg = numeric(0),
-                  maxdelta = numeric(0),
-                  max2delta = numeric(0))
-for (id in tsh_case$id) {
+                  cohort = character(0),
+                  tshResult = numeric(0),
+                  statinType = character(0),
+                  avgBaseFG = numeric(0),
+                  deltaFG = integer(0),
+                  maxDelta = integer(0),
+                  sex = character(0),
+                  age = integer(0),
+                  race = character(0),
+                  hispanic = character(0),
+                  diabetes = integer(0),
+                  diagFromFG = integer(0),
+                  diagFromICD = integer(0),
+                  group = character(0))
+for (id in sort(tshCntl$id)) {
   data[nrow(data) + 1,] = c(id,
+                            "control",
+                            tshCntl$tshresult[tshCntl$id == id],
+                            glucoseCntl$GENERIC_NAME[glucoseCntl$id == id],
+                            glucoseCntl$avepre_gluresult[glucoseCntl$id == id],
+                            glucoseCntl$deltafg[glucoseCntl$id == id],
+                            glucoseCntl$maxdelta[glucoseCntl$id == id],
+                            glucoseCntl$PATIENT_SEX[glucoseCntl$id == id],
+                            glucoseCntl$PATIENT_AGE[glucoseCntl$id == id],
+                            glucoseCntl$CATEGORY.OF.KPNC.RACE[
+                              glucoseCntl$id == id],
+                            glucoseCntl$Is.Member.Hispanic...KPNC.variable.[
+                              glucoseCntl$id == id],
+                            0,
+                            0,
+                            0,
+                            3)
+}
+for (id in sort(tshCase$id)) {
+  data[nrow(data) + 1,] = c(200000 + id,
                             "case",
-                            tsh_case$tshresult[tsh_case$id == id],
-                            glucose_case$deltafg[glucose_case$id == id],
-                            glucose_case$maxdelta[glucose_case$id == id])
+                            tshCase$tshresult[tshCase$id == id],
+                            "",
+                            glucoseCase$avepre_gluresult[glucoseCase$id == id],
+                            glucoseCase$deltafg[glucoseCase$id == id],
+                            glucoseCase$maxdelta[glucoseCase$id == id],
+                            glucoseCase$PATIENT_SEX[glucoseCase$id == id],
+                            glucoseCase$PATIENT_AGE[glucoseCase$id == id],
+                            glucoseCase$CATEGORY.OF.KPNC.RACE[
+                              glucoseCase$id == id],
+                            glucoseCase$Is.Member.Hispanic...KPNC.variable.[
+                              glucoseCase$id == id],
+                            glucoseCase$diab.comb[glucoseCase$id == id],
+                            glucoseCase$diab.from.FG[glucoseCase$id == id],
+                            glucoseCase$diab.from.ICD[glucoseCase$id == id],
+                            glucoseCase$group[glucoseCase$id == id])
 }
-for (id in tsh_cntl$id) {
-  data[nrow(data) + 1,] = c(id,
-                            "cntl",
-                            tsh_cntl$tshresult[tsh_cntl$id == id],
-                            glucose_cntl$deltafg[glucose_cntl$id == id],
-                            glucose_cntl$maxdelta[glucose_cntl$id == id])
+data$diabetes = data$diabetes == 1
+data$diagFromFG = data$diagFromFG == 1
+data$diagFromICD = data$diagFromICD == 1
+data$pdd = NA
+for (id in rxCntl$IDs) {
+  data$pdd[data$id == id] = rxCntl$PDD.DDD[rxCntl$IDs == id]
 }
-data$hypothyroidism = data$tsh_result > 5.5
+for (id in rxCase$IDs) {
+  data$statinType[data$id == id] = rxCase$Primary.Drug[rxCase$IDs == id]
+  data$pdd[data$id == id] = rxCase$PDD.DDD[rxCase$IDs == id]
+}
+data$hypothyroidism = data$tshResult > 5.5
+
+write.csv(data, "data/TSH_FG_data.csv")
